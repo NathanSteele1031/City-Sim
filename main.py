eb7fe3571b2935eb7c4d4a559e1a7601404d787c
Nathan Steele <nsteele1031@gmail.com>
2025-11-30 14:16:58 -0500

    Added simple system for roads

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-import pygame, colors
+import pygame, colors, roads
 
 def main():
     pygame.init()
     display = pygame.display.set_mode((800, 600))
     running = True
+    road_mode = False
+    sample_road = None
+
+    buildings = []
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r: # Activate placing roads
+                    road_mode = not road_mode
+
+            if event.type == pygame.MOUSEBUTTONDOWN and road_mode: # Place road
+                if not sample_road:
+                    sample_road = roads.Road(*pygame.mouse.get_pos(), 0, 0)
+                else:
+                    sample_road.set_ending_point(*pygame.mouse.get_pos())
+                    buildings.append(sample_road)
+                    sample_road = None
+
         display.fill(colors.GREEN)
+
+        if sample_road:
+            sample_road.set_ending_point(*pygame.mouse.get_pos())
+            sample_road.draw(display)
+
+        for building in buildings:
+            building.draw(display)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
