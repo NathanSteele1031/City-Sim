eddb27e7408fdc2c85a5a39791bd87d223719c61
Nathan Steele <nsteele1031@gmail.com>
2025-12-06 07:59:24 -0500

    Added setters and getters

--- buildings.py
+++ buildings.py
@@ -4,14 +4,19 @@ class Building:
     def __init__(self, x, y, rotation):
         self.x = x
         self.y = y
-        self.rotation = rotation
+        self._rotation = rotation
 
-    def update_rotation(self, rotation):
-        self.rotation = rotation % 360
+    @property
+    def rotation(self):
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, rotation):
+        self._rotation = rotation % 360
     
     def draw(self, screen: pygame.Surface): 
         # Need to know the valid place to put buildings on roads
         # Need to know the rotation so the buildings are alined with the roads
         buildingImg = pygame.image.load("Images/BlankBuilding.png")
-        buildingImg = pygame.transform.rotate(buildingImg, self.rotation)
+        buildingImg = pygame.transform.rotate(buildingImg, self._rotation)
         screen.blit(buildingImg, (self.x, self.y))
