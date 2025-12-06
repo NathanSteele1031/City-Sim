433ec3fd41af694329038c5e4b4bc23e113fc812
Nathan Steele <nsteele1031@gmail.com>
2025-12-06 07:54:39 -0500

    Added rotation values and setter

--- buildings.py
+++ buildings.py
@@ -1,12 +1,17 @@
-import pygame, colors
+import pygame, colors, roads
 
 class Building:
-    def __init__(self, x, y):
+    def __init__(self, x, y, rotation):
         self.x = x
         self.y = y
+        self.rotation = rotation
+
+    def update_rotation(self, rotation):
+        self.rotation = rotation % 360
     
     def draw(self, screen: pygame.Surface): 
         # Need to know the valid place to put buildings on roads
         # Need to know the rotation so the buildings are alined with the roads
-        rect_value = pygame.rect.Rect((self.x, self.y), (10, 10))
-        pygame.draw.rect(screen, colors.BLACK, (self.x, self.y, 10, 10))
+        buildingImg = pygame.image.load("Images/BlankBuilding.png")
+        buildingImg = pygame.transform.rotate(buildingImg, self.rotation)
+        screen.blit(buildingImg, (self.x, self.y))
